215a3080b2afb5056135bfff48d42df2be9de46f
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2020-05-13 16:28:21 +0200

    Comments and sigint

--- cmd/war/main.go
+++ cmd/war/main.go
@@ -4,33 +4,42 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 
 	"github.com/doctordesh/war"
 	"github.com/doctordesh/war/colors"
 )
 
-func main() {
+var usage = func() {
+	fmt.Println("Usage: war [options] <command-to-run>")
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+}
 
+func main() {
+	// flag variables
 	var delay int
 	var match, exclude string
 	var verbose, boring bool
 
-	flag.Usage = func() {
-		fmt.Println("Usage: war [options] <command-to-run>")
-		fmt.Println("Options:")
-		flag.PrintDefaults()
-	}
+	// setup flags
 	flag.IntVar(&delay, "delay", 100, "Time in milliseconds before running command. Events within the delay will reset the delay")
 	flag.StringVar(&match, "match", "*", "Match files, separate with comma")
 	flag.StringVar(&exclude, "exclude", "", "Pattern to exclude files, separate with comma")
 	flag.BoolVar(&verbose, "verbose", false, "Verbose output")
 	flag.BoolVar(&boring, "boring", false, "Boring (no colors) output")
 
+	// usage of the program
+	flag.Usage = usage
+
 	flag.Parse()
 
+	// set the coloring
 	colors.SetColoring(!boring)
 
+	// Validate that we got the path
 	args := flag.Args()
 	if len(args) == 0 {
 		colors.Red("missing <command> argument")
@@ -38,27 +47,44 @@ func main() {
 		os.Exit(2)
 	}
 
+	// Warn about unnecessary arguments
 	if len(args) > 1 {
 		colors.Yellow("ignores arguments '%v'", strings.Join(args[1:], "', '"))
 	}
 
+	// Get the current working directory to know what to watch
 	path, err := os.Getwd()
 	if err != nil {
 		colors.Red("could not find current working directory: %v", err)
 		os.Exit(2)
 	}
 
+	// Extract matches and excludes from the string format
 	matches := splitAndTrim(match)
 	excludes := splitAndTrim(exclude)
 
+	// Build program
 	w := war.New(path, matches, excludes, args[0], os.Environ(), delay)
 	w.Verbose = verbose
 
-	err = w.WatchAndRun()
-	if err != nil {
-		colors.Red("could not start war: %v", err)
-		os.Exit(2)
-	}
+	// Setup signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+
+	// Run
+	go func() {
+		err = w.WatchAndRun()
+		if err != nil {
+			colors.Red("could not start war: %v", err)
+			os.Exit(2)
+		}
+	}()
+
+	<-sigs
+
+	fmt.Println()
+	colors.Blue("keyboard interrupt detected, quiting...")
+	os.Exit(0)
 }
 
 func splitAndTrim(s string) []string {
